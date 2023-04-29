226ad3a041d5b81a75c1b016d8006ef412fdc089
ehmoratallar8990 <ehmoratallar@gmail.com>
2023-04-29 14:51:40 -0600

    Funcionalidad Filtro

--- src/components/ComboPrecio.vue
+++ src/components/ComboPrecio.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="row justify-center">
-        <div class="flex flex-col xs6 lg2 ">
+        <div class="flex flex-col xs7 lg2 contenedor-izquierda">
             <h3>{{ nombre }}</h3>
-            <p>{{ descripcion }}</p>
+            <p class="descripcion">{{ descripcion }}</p>
             <button>Q.{{ precio }}</button>
         </div>
-        <div class="flex flex-col xs6 lg2">
-            <img :src="imagen" />
+        <div class="flex flex-col xs5 lg2">
+            <img class="imagen-producto" :src="imagen" />
         </div>
     </div>
 </template>
 
 <script>
-    export default {
+    export default{
         name: "ComboPrecio",
         props: {
             nombre: String,
@@ -23,22 +23,37 @@
     }
 </script>
 
-<style>
+<style scoped>
+    .contenedor-izquierda {
+        padding: 25px;
+        text-align: left;
+    }
     h3 {
-        font-family: 'Roboto Flex';
+        font-family: 'Roboto';
         font-style: normal;
         font-weight: 700;
         font-size: 20px;
-        line-height: 50px;
+        line-height: 1.2;
+        margin-bottom: 8px;
+        /* line-height: 50px; */
         /* identical to box height, or 250% */
-
+        /* margin-bottom: 5px; */
         color: #000000;
     }
-    p {
-        margin-bottom: 5px;
+    p.descripcion {
+        margin-bottom: 25px;
+        /* padding: 0 25px; */
     }
     button {
         background: #EDAF11;
         padding: 2px 20px;
+    }
+    .imagen-producto {
+        height: 143px;
+        width: 143px;
+        object-fit: contain;
+        background-color: white;
+        padding: 15px;
+        border-radius: 5px;
     }
 </style>
--- src/components/PaginaMenu.vue
+++ src/components/PaginaMenu.vue
@@ -1,9 +1,39 @@
 <template>
     <div>
         <SlideMenu />
+        <div class="contenedor-filtros mb-10">
+            <va-button
+                :color="filtro === 'combo' ? 'danger' : 'warning'"
+                class="mr-6 mb-2"
+                @click="setFiltro('combo')"
+            >
+                Combo
+            </va-button>
+            <va-button
+                :color="filtro === 'individual' ? 'danger' : 'warning'"
+                class="mr-6 mb-2"
+                @click="setFiltro('individual')"
+            >
+                Individual
+            </va-button>
+            <va-button
+                :color="filtro === 'bebida' ? 'danger' : 'warning'"
+                class="mb-2"
+                @click="setFiltro('bebida')"
+            >
+                Bebidas
+            </va-button>
+        </div>
         <div class="grid grid-cols-1 items-start">
-            <ComboPrecio nombre="Combo 1" descripcion="Combo de hamburguesa" precio="45.00" imagen="combo1.png"></ComboPrecio>
-            <ComboPrecio nombre="Combo 2" descripcion="Combo de hamburguesa" precio="45.00" imagen="combo1.png"></ComboPrecio>
+            <ComboPrecio 
+                class="item-lista"
+                v-for="item in itemsFiltrados" 
+                :key="`item-${item.id}`" 
+                :nombre="item.nombre" 
+                :descripcion="item.descripcion" 
+                :precio="item.precio" 
+                :imagen="item.imagen"
+            ></ComboPrecio>
         </div>
     </div>
 </template>
@@ -15,7 +45,95 @@
         components: {
             SlideMenu,
             ComboPrecio
-        }
+        },
+        data() {
+            return {
+                filtro: 'combo',
+                itemsFiltrados: [],
+                itemsMenu: [
+                    {
+                        id: 1,
+                        nombre: "Combo 1",
+                        descripcion: "Hamburguesa con papas y bebida",
+                        tipo: "combo",
+                        precio: 45.00,
+                        imagen: "combo1.png"
+                    },
+                    {
+                        id: 2,
+                        nombre: "Combo 2",
+                        descripcion: "Hamburguesa con papas, bebida y postre",
+                        tipo: "combo",
+                        precio: 55.00,
+                        imagen: "combo1.png"
+                    },
+                    {
+                        id: 3,
+                        nombre: "Combo 3",
+                        descripcion: "Pizza personal bebida y postre",
+                        tipo: "combo",
+                        precio: 55.00,
+                        imagen: "pizza.jpg"
+                    },
+                    {
+                        id: 4,
+                        nombre: "Pizza individual personal",
+                        descripcion: "Pizza individual personal del sabor de su elección",
+                        tipo: "individual",
+                        precio: 25.00,
+                        imagen: "pizza.jpg"
+                    },
+                    {
+                        id: 5,
+                        nombre: "Hamburguesa simple",
+                        descripcion: "Deliciosa hamburguesa hecha con ingredientes frescos",
+                        tipo: "individual",
+                        precio: 25.00,
+                        imagen: "combo1.png"
+                    },
+                    {
+                        id: 6,
+                        nombre: "Bebida en lata",
+                        descripcion: "Coca cola, sprite, fanta o pepsi",
+                        tipo: "bebida",
+                        precio: 5.00,
+                        imagen: "latas.jpg"
+                    },
+                    {
+                        id: 7,
+                        nombre: "Bebida en botella",
+                        descripcion: "Coca cola, sprite, fanta o pepsi",
+                        tipo: "bebida",
+                        precio: 10.00,
+                        imagen: "sodabotella.jpg"
+                    },
+                    {
+                        id: 8,
+                        nombre: "Bebida natural",
+                        descripcion: "Rosa de Jamaica, naranjada o limonada",
+                        tipo: "bebida",
+                        precio: 10.00,
+                        imagen: "bebidasnaturales.jpg"
+                    },
+                ], 
+            };
+        },
+        methods: {
+            filtrarMenu() {
+                this.itemsFiltrados = this.itemsMenu.filter((item) => item.tipo === this.filtro);
+            },
+            setFiltro(filtro) {
+                this.filtro = filtro;
+            }
+        },
+        watch: {
+            filtro() {
+                this.filtrarMenu();
+            },
+        },
+        mounted() {
+            this.filtrarMenu();
+        },
     }
 </script>
 
@@ -62,5 +180,14 @@
     .button-container {
         width: 100%;
 
+    }
+    .contenedor-filtros {
+        margin-bottom: 25px;
+        display: flex;
+        gap: 25px;
+        justify-content: center;
+    }
+    .item-lista {
+        padding: 15px 0;
     }
 </style>
