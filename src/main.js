8f548d4e94fda10a96a1ffdf9caa0f965359668b
Cinthia Rodriguez <crodriguez_ges@galileo.edu>
2023-05-13 12:49:07 -0600

    Agregando MITT.

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import App from "./App.vue";
 import { createVuestic } from "vuestic-ui";
 import "vuestic-ui/css";
 import router from './router'
+import mitt from 'mitt'
+
+const emitter = mitt()
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 import store from './store'
 
-createApp(App).use(store).use(router).use(createVuestic()).mount("#app");
+const app = createApp(App).use(store).use(router).use(createVuestic());
+
+app.config.globalProperties.emitter = emitter
+app.mount('#app')
